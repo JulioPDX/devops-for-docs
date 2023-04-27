641059de20554aa1743abd4440f635d8d3cc081c
juliopdx <juliop503@gmail.com>
2023-04-27 14:36:55 -0700

    updating the build steps

---
+++ tags.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git fetch --tags
+
+list="$(git tag)"
+
+# deploy all tags
+echo "$list" | while read tag
+do
+    git checkout tags/$tag
+    mike deploy $tag
+
+done
+
+# deploy main or devel and set the latest stable
+
+mike checkout main
+mike deploy devel
+mike set-default --push $(git describe --tags)
